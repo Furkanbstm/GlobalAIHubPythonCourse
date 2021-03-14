d0c7e4b59ed8771e49b67e1c083451fdd386c701
Furkanbstm <77301823+Furkanbstm@users.noreply.github.com>
2021-03-14 20:19:20 +0300

    Create hw2.py

---
+++ hub/hw1,2,3/hw2.py
@@ -0,0 +1,15 @@
+d_0= {"AD-SOYAD":"ALİ HAN","ÜNİVERSİTE":"ODTÜ","ORT":"3.7","ALAN":"HR"}
+d_1= {"AD-SOYAD":"CAN YILMAZ","ÜNİVERSİTE":"GAZİ ÜNİ.","ORT":"3.4","ALAN":"SALES"}
+d_2= {"AD-SOYAD":"MEHMET TARAN","ÜNİVERSİTE":"YTÜ","ORT":"3.5","ALAN":"BD"}
+d_3= {"AD-SOYAD":"YILDIZ KILIÇ","ÜNİVERSİTE":"İTÜ","ORT":"3.1","ALAN":"IT"}
+d_4= {"AD-SOYAD":"İPEK ÖZ","ÜNİVERSİTE":"KTÜ","ORT":"2.9","ALAN":"PRODUCT MANAGEMENT"}
+for L in d_0.values():
+  print(L)
+for L in d_1.values():
+  print(L)
+for L in d_2.values():
+  print(L)
+for L in d_3.values():
+  print(L)
+for L in d_4.values():
+  print(L)
